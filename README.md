44427bb42dbe04bf0edc4d0da1e5502263bbdf73
Stephen Clarke <sclarke81@gmail.com>
2019-01-11 16:56:28 +0000

    Build core images as well

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Use the following to run Jira. Once running, it can be accessed at http://localh
 ```powershell
 docker volume create --name jiraDataVolume
 docker volume create --name jiraLogVolume
-docker run -d --name jira -p 8080:8080 -v jiraLogVolume:C:\tools\jira\logs -v jiraDataVolume:C:\data\jira sclarke81/jira:7.13.0-nanoserver
+docker run -d --name jira -p 8080:8080 -v jiraLogVolume:C:\tools\jira\logs -v jiraDataVolume:C:\data\jira sclarke81/jira:7.13.0-nanoserver-1803
 ```
--- build.ps1
+++ build.ps1
@@ -1,2 +1,7 @@
-docker build --tag sclarke81/openjdk:jdk8u192-b12-nanoserver .\openjdk
-docker build --tag sclarke81/jira:7.13.0-nanoserver .\jira
+docker build --tag sclarke81/openjdk:jdk8u192-b12-nanoserver-sac2016 .\openjdk
+$openJdkCore = (docker image ls --filter "dangling=true" -q)[0]
+docker tag $openJdkCore sclarke81/openjdk:jdk8u192-b12-windowsservercore-ltsc2016
+
+docker build --tag sclarke81/jira:7.13.0-nanoserver-sac2016 .\jira
+$jiraCore = (docker image ls --filter "dangling=true" -q)[0]
+docker tag $jiraCore sclarke81/jira:7.13.0-windowsservercore-ltsc2016
